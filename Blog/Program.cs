8c66224aa33b1871d89c6be69782f7a91cc4e251
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-07 20:27:15 -0300

    Criando o projeto

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.Data.SqlClient;
+
+internal class Program
+{
+    private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=Dan@8257";
+    private static void Main(string [] args)
+    {
+    }
+
+    public static void ReadUsers()
+    {
+        using (var connection = new SqlConnection())
+        {
+
+        }
+    }
+
+}
