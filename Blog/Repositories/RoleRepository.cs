3bf629db5365467edbc0209cb60aefeff9588b8c
Danilo Carvalho <danilocarvalhosp@gmail.com>
2023-08-08 19:57:19 -0300

    Otimizando o código (RoleRepository)

--- Blog/Repositories/RoleRepository.cs
+++ Blog/Repositories/RoleRepository.cs
@@ -3,6 +3,7 @@ using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,26 @@ namespace Blog.Repositories
 
         public void Create(Role role)
             => _connection.Insert<Role>(role);
+
+        public void Update(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Update<Role>(role);
+        }
+
+        public void Delete(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Delete<Role>(role);
+        }
+
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return;
+
+            var role = _connection.Get<Role>(id);
+            _connection.Delete<Role>(role);
+        }
     }
 }
